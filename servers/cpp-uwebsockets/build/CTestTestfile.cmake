6a0a891d82c2f17c5217309b64898cb46e776a0a
pyrareae <pyrareae@gmail.com>
2022-01-30 02:26:05 -0800

    rm build

--- servers/cpp-uwebsockets/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pyra/code/stoat_chat
-# Build directory: /home/pyra/code/stoat_chat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
